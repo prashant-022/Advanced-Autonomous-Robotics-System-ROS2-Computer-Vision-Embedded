b6c5c4198a090ee19c91a567eb8cc186ee421c11
Prashant Kumar <pkumarofficial02@gmail.com>
2026-04-19 18:30:30 +0530

    added imu node

---
+++ ros2_ws/src/imu_pkg/imu_pkg/imu_node.py
@@ -0,0 +1,111 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Imu
+import smbus
+import time
+import math
+import tf_transformations
+
+class IMUNode(Node):
+    def __init__(self):
+        super().__init__('imu_node')
+
+        self.publisher = self.create_publisher(Imu, '/imu', 10)
+
+        self.bus = smbus.SMBus(1)
+        self.addr = 0x68
+        self.bus.write_byte_data(self.addr, 0x6B, 0)
+
+        self.alpha = 0.98
+        self.angle = 0
+        self.yaw = 0
+
+        self.prev_time = time.time()
+
+        self.calibrate()
+
+        self.timer = self.create_timer(0.02, self.loop)
+
+    def read_word(self, reg):
+        high = self.bus.read_byte_data(self.addr, reg)
+        low = self.bus.read_byte_data(self.addr, reg+1)
+        value = (high << 8) + low
+        if value >= 0x8000:
+            value = -((65535 - value) + 1)
+        return value
+
+    def get_accel(self):
+        return (
+            self.read_word(0x3B)/16384.0,
+            self.read_word(0x3D)/16384.0,
+            self.read_word(0x3F)/16384.0
+        )
+
+    def get_gyro(self):
+        return (
+            self.read_word(0x43)/131.0,
+            self.read_word(0x45)/131.0,
+            self.read_word(0x47)/131.0
+        )
+
+    def calibrate(self):
+        self.get_logger().info("Calibrating IMU...")
+
+        gx_off = gy_off = gz_off = 0
+        for _ in range(500):
+            gx, gy, gz = self.get_gyro()
+            gx_off += gx
+            gy_off += gy
+            gz_off += gz
+            time.sleep(0.005)
+
+        self.gx_off = gx_off / 500
+        self.gy_off = gy_off / 500
+        self.gz_off = gz_off / 500
+
+    def loop(self):
+        msg = Imu()
+
+        current_time = time.time()
+        dt = current_time - self.prev_time
+        self.prev_time = current_time
+
+        ax, ay, az = self.get_accel()
+        gx, gy, gz = self.get_gyro()
+
+        gx -= self.gx_off
+        gy -= self.gy_off
+        gz -= self.gz_off
+
+        # Complementary filter
+        acc_angle = math.atan2(ay, az)
+        gyro_angle = self.angle + math.radians(gx) * dt
+        self.angle = self.alpha * gyro_angle + (1 - self.alpha) * acc_angle
+
+        self.yaw += gz * dt
+
+        # Fill ROS message
+        msg.angular_velocity.x = gx
+        msg.angular_velocity.y = gy
+        msg.angular_velocity.z = gz
+
+        msg.linear_acceleration.x = ax
+        msg.linear_acceleration.y = ay
+        msg.linear_acceleration.z = az
+
+        # Simple orientation (only roll)
+        q = tf_transformations.quaternion_from_euler(self.angle, 0, 0)
+        msg.orientation.x = q[0]
+        msg.orientation.y = q[1]
+        msg.orientation.z = q[2]
+        msg.orientation.w = q[3]
+
+        self.publisher.publish(msg)
+
+
+def main():
+    rclpy.init()
+    node = IMUNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
