5c3651f3ac70d7a8ff5ca7225c2d97feb3101b7c
Prashant Kumar <pkumarofficial02@gmail.com>
2026-05-02 10:55:47 +0530

    Updated EKF parameters for Sensor Fusion

--- ros2_ws/src/robot_bringup/launch/robot.launch.py
+++ ros2_ws/src/robot_bringup/launch/robot.launch.py
@@ -32,4 +32,33 @@ def generate_launch_description():
             output='screen'
         ),  
         
+        Node(
+            package='serial_pkg',
+            executable='encoder_node',
+            name='encoder_node',
+            output='screen'
+        ), 
+        
+        Node(
+            package='control_pkg',
+            executable='odometry_node',
+            name='odometry_node',
+            output='screen'
+        ), 
+        
+        Node(
+            package='imu_pkg',
+            executable='imu_node',
+            name='imu_node',
+            output='screen'
+        ), 
+        
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=['/home/pras/ros2_ws/src/robot_bringup/config/ekf.yaml']
+        ), 
+        
     ])
